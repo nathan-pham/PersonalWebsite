757c7a62354b0be63d147a6f7b922169ded0bc63
Nathan Pham <nathanpham.me@gmail.com>
2020-10-25 01:00:16 -0700

    header introduced modules, created transitioning header

--- components/header/header.js
+++ components/header/header.js
@@ -1,10 +1,51 @@
-const Header = () => {
-  return (
-    <header className="header flex between">
-      <img src="/icons/apple-icon.png" alt="N Logo" />
-      <div></div>
-    </header>
-  );
-};
+import React, { Component } from "react";
+import styles from "./header.module.css";
+
+class Modal extends Component {
+  render() {
+    let classes = ["header-padding", styles.modal];
+    if (this.props.show) {
+      classes.push(styles.show);
+    }
+
+    return (
+      <div className={classes.join(" ")}>
+        <h1 className="title">Hello World</h1>
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+    this.showModal = this.showModal.bind(this);
+  }
+  showModal() {
+    this.setState((state) => {
+      return {
+        show: !state.show
+      };
+    });
+  }
+  render() {
+    return (
+      <>
+        <header
+          className={`${styles.header} flex justify-between align-center`}
+        >
+          <img src="/icons/apple-icon.png" alt="N Logo" />
+          <div onClick={this.showModal}>
+            <div className={styles.hamburger}></div>
+          </div>
+        </header>
+        <Modal show={this.state.show} />
+      </>
+    );
+  }
+}
 
 export default Header;
