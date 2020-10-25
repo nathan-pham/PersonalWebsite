63076fb7b4ca1ef20bcb6256801943dee48ee84d
Nathan Pham <nathanpham.me@gmail.com>
2020-10-25 01:06:42 -0700

    header resources studying react css transitions

--- components/header/header.js
+++ components/header/header.js
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+// import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+//https://medium.com/@joethedave/achieving-ui-animations-with-react-the-right-way-562fa8a91935
+//https://reactjs.org/docs/animation.html
+//https://blog.bitsrc.io/build-a-simple-modal-component-with-react-16decdc111a6
+//https://medium.com/atheros/fade-in-and-fade-out-animation-with-spring-and-transition-react-hooks-114eddbd74a7
 import styles from "./header.module.css";
 
 class Modal extends Component {
